61436e6768189b8c7da221bfb238396807dc9120
f-istvan <f.istvan1984@gmail.com>
2017-11-07 21:22:50 +0100

    refactor unit tests

--- test/hello_world/test/core.clj
+++ test/hello_world/test/core.clj
@@ -2,30 +2,21 @@
   (:use [hello-world.core])
   (:use [clojure.test]))
 
-(deftest replace-mr
-  (is (= (log-search [] 1) nil) ))
+(deftest nil-tests
+  (testing "empty-sequence"
+    (is (= (log-search [] 1) nil) ))
+  (testing "element-not-present"
+    (is (= (log-search [1, 2] 3) nil) )))
 
-(deftest replace-me1
-  (is (= (log-search [1, 2] 3) nil) ))
+(deftest element-present-tests
+  (testing "one-length-sequence"
+    (is (= (log-search [3] 3) 0) ))
+  (testing "multi-elem-sequence"
+    (is (= (log-search [1, 5, 6] 1) 0) )
+    (is (= (log-search [1, 5, 6] 5) 1) )
+    (is (= (log-search [1, 5, 6] 6) 2) )))
 
-(deftest replace-me2
-  (is (= (log-search [3] 3) 0) ))
-
-(deftest replace-me3
-  (is (= (log-search [1, 5, 6] 1) 0) ))
-
-(deftest replace-me4
-  (is (= (log-search [1, 5, 6] 5) 1) ))
-
-(deftest replace-me5
-  (is (= (log-search [1, 5, 6] 6) 2) ))
-
-
-(deftest replace-me6
-  (is (= (log-search [1, 5, 5, 6] 5) 1) ))
-
-(deftest replace-me7
-  (is (= (log-search [1, 1, 5, 6] 1) 1) ))
-
-(deftest replace-me8
+(deftest contains-same-elements-tests
+  (is (= (log-search [1, 5, 5, 6] 5) 1) )
+  (is (= (log-search [1, 1, 5, 6] 1) 1) )
   (is (= (log-search [1, 2, 6, 6] 6) 2) ))
