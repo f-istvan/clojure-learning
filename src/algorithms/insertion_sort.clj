63a50139651d8075f672f9c640b106f02bfad372
f-istvan <f.istvan1984@gmail.com>
2017-11-19 13:16:58 +0100

    add more tests, cut down one iteration

--- src/algorithms/insertion_sort.clj
+++ src/algorithms/insertion_sort.clj
@@ -1,19 +1,17 @@
 (ns algorithms.insertion-sort)
 
-
 (defn insert
   [l k]
   (concat (filter #(<= % k) l) [k] (filter #(> % k) l)))
 
 (defn insertion-sort
   [input]
-  (loop [i 0
-         r input]
-    (println i)
-    (if (>= i (count input))
-      r
+  (loop [i 1
+         result input]
+    ;; (println result) ;; uncomment to see how insertion works
+    (if (>= i (count result))
+      result
       (recur (inc i)
              (concat
-              (insert (take i r)
-                      (nth r i))
-              (take-last (- (- (count input) i) 1) r))))))
+               (insert (take i result) (nth result i))
+               (take-last (- (- (count result) i) 1) result))))))
--- test/algorithms/test/insertion_sort.clj
+++ test/algorithms/test/insertion_sort.clj
@@ -8,3 +8,12 @@
 
 (deftest init-test-3
   (is (= (insertion-sort [5 3 4 2 1 1] ) [1 1 2 3 4 5])))
+
+(deftest init-test-4
+  (is (= (insertion-sort [0 0 0] ) [0 0 0])))
+
+(deftest init-test-5
+  (is (= (insertion-sort [9] ) [9])))
+
+(deftest init-test-6
+  (is (= (insertion-sort [1 -1 0 -2 -4 5 7] ) [-4 -2 -1 0 1 5 7])))
